267fc1b3700476f1455f3a4bbdf463528c8fb4fc
Brian <brian.du@zoocasa.com>
2017-08-02 15:03:35 -0400

    Style top nav and social media icons
    Add links
    Remove bullet points on links
    Style nav bar links
    Added Montserrat font
    Style nav bar
    Add get in touch component
    Reposition links
    Add get in touch button
    Add social media icons
    Style and position social media icons

--- tests/integration/pods/components/get-in-touch/component-test.js
+++ tests/integration/pods/components/get-in-touch/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('top-nav', 'Integration | Component | top nav', {
+moduleForComponent('get-in-touch', 'Integration | Component | get in touch', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{top-nav}}`);
+  this.render(hbs`{{get-in-touch}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#top-nav}}
+    {{#get-in-touch}}
       template block text
-    {{/top-nav}}
+    {{/get-in-touch}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
